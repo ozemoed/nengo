864b14e2fce09c154a23631682c9bce539a0cf68
Jan Gosmann <jgosmann@uwaterloo.ca>
2017-03-08 13:55:47 -0500

    Add test that optimizer does not change result.

--- nengo/builder/tests/test_optimizer.py
+++ nengo/builder/tests/test_optimizer.py
@@ -1,8 +1,11 @@
 import numpy as np
+from numpy.testing import assert_almost_equal
 import pytest
 
+import nengo
 from nengo.builder.optimizer import SigMerger
 from nengo.builder.signal import Signal
+from nengo import spa
 
 
 def test_sigmerger_check():
@@ -113,3 +116,60 @@ def test_sigmerger_merge_views():
     assert np.allclose(merged.initial_value, s.initial_value)
     assert v1.base is s
     assert v2.base is s
+
+
+def test_optimizer_does_not_change_result(RefSimulator, plt, seed):
+    with spa.SPA(seed=seed) as model:
+        model.vision = spa.State(dimensions=16, neurons_per_dimension=80)
+        model.vision2 = spa.State(dimensions=16, neurons_per_dimension=80)
+        model.motor = spa.State(dimensions=16, neurons_per_dimension=80)
+        model.motor2 = spa.State(dimensions=32, neurons_per_dimension=80)
+
+        actions = spa.Actions(
+            'dot(vision, A) --> motor=A, motor2=vision*vision2',
+            'dot(vision, B) --> motor=vision, motor2=vision*A*~B',
+            'dot(vision, ~A) --> motor=~vision, motor2=~vision*vision2'
+        )
+        model.bg = spa.BasalGanglia(actions)
+        model.thalamus = spa.Thalamus(model.bg)
+
+        def input_f(t):
+            if t < 0.1:
+                return 'A'
+            elif t < 0.3:
+                return 'B'
+            elif t < 0.5:
+                return '~A'
+            else:
+                return '0'
+        model.input = spa.Input(vision=input_f, vision2='B*~A')
+
+        input, vocab = model.get_module_input('motor')
+        input2, vocab2 = model.get_module_input('motor2')
+        p = nengo.Probe(input, 'output', synapse=0.03)
+        p2 = nengo.Probe(input2, 'output', synapse=0.03)
+
+    with RefSimulator(model, optimize=False) as sim:
+        sim.run(0.5)
+    with RefSimulator(model, optimize=True) as sim_opt:
+        sim_opt.run(0.5)
+
+    t = sim.trange()
+    data = vocab.dot(sim.data[p].T)
+    data2 = vocab2.dot(sim.data[p2].T)
+
+    plt.subplot(2, 2, 1)
+    plt.plot(t, data.T)
+    plt.subplot(2, 2, 2)
+    plt.plot(t, data2.T)
+
+    data_opt = vocab.dot(sim_opt.data[p].T)
+    data_opt2 = vocab2.dot(sim_opt.data[p2].T)
+
+    plt.subplot(2, 2, 3)
+    plt.plot(t, data_opt.T)
+    plt.subplot(2, 2, 4)
+    plt.plot(t, data_opt2.T)
+
+    assert_almost_equal(sim.data[p], sim_opt.data[p])
+    assert_almost_equal(sim.data[p2], sim_opt.data[p2])
